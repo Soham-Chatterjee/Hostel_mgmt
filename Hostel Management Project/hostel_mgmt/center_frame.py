e8f725a9adedc097911a1c94cd52ac311b6e08fd
Soham Chatterjee <sohamchatterjee2324@gmail.com>
2020-12-02 11:09:05 +0530

    Updated

--- Hostel Management Project/hostel_mgmt/center_frame.py
+++ Hostel Management Project/hostel_mgmt/center_frame.py
@@ -10,4 +10,4 @@ pos_x = int((center_frame.winfo_width()/2)-(home_text.winfo_width()/2))
 pos_y = int((center_frame.winfo_height()/2)-(home_text.winfo_height()/2))
 home_text['fg'] = "bisque4"
 home_text.place(x=pos_x, y=pos_y)
-root.update()
+root.update(
