56550683bf21c21f6a25c0cc430f86964b2f652e
Soham Chatterjee <sohamchatterjee2324@gmail.com>
2020-12-02 11:07:26 +0530

    Center Frame Configuration

--- Hostel Management Project/hostel_mgmt/center_frame.py
+++ Hostel Management Project/hostel_mgmt/center_frame.py
@@ -2,5 +2,12 @@ import tkinter
 from hostel_mgmt import root
 from hostel_mgmt.design import center_frame
 
-home_text = tkinter.Label(center_frame, text="RISING STAR GROUP OF HOSTELS\nRECORDS MANAGEMENT DATABASE", bg="bisque3", fg="bisque4", font=('Courier New', 30))
-home_text.place(x=44.4, y=320)
+
+home_text = tkinter.Label(center_frame, text="RISING STAR GROUP OF HOSTELS\nRECORDS MANAGEMENT DATABASE", bg="bisque3", fg="bisque3", font=('Courier New', 30))
+home_text.place(x=100, y=100)
+root.update()
+pos_x = int((center_frame.winfo_width()/2)-(home_text.winfo_width()/2))
+pos_y = int((center_frame.winfo_height()/2)-(home_text.winfo_height()/2))
+home_text['fg'] = "bisque4"
+home_text.place(x=pos_x, y=pos_y)
+root.update()
